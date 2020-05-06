846fff6c28b4349cbcd971fd8492f4ee0d91c348
jjanmo <jjanmo07@hanmail.net>
2020-05-07 01:51:52 +0900

    Add mine sweeper game html and css

---
+++ mineSweeper/mineSweeper.js
@@ -0,0 +1,28 @@
+const board = document.getElementById('js-board');
+
+
+const blockObjs = [];
+
+/*
+-
+- isMine : boolean
+- 
+
+*/
+
+
+
+
+
+//화면과 데이터의 일치
+
+
+
+
+
+function init() {
+
+}
+
+
+init();
