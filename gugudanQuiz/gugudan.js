624ac657334db911f64ff48ffdf3c92883f23b23
jjanmo <44132977+jjanmo@users.noreply.github.com>
2020-08-14 18:17:12 +0900

    Update code style of gugudan game

--- gugudanQuiz/gugudan.js
+++ gugudanQuiz/gugudan.js
@@ -1,42 +1,33 @@
-const readyContainer = document.querySelector(".readyContainer");
-const startBtn = readyContainer.querySelector(".startBtn");
-
-const inputForm = document.querySelector("#inputForm");
-const input = inputForm.querySelector("input");
-
-const btnContainer = document.querySelector(".btnContainer");
-const nextBtn = btnContainer.querySelector(".nextBtn");
-const quitBtn = btnContainer.querySelector(".quitBtn");
-
-//score span
-const right = document.querySelector("#right");
-const wrong = document.querySelector("#wrong");
-//result div
-const result = document.querySelector("#result");
-//progress bar
-const bar = document.querySelector("#bar");
-
+const readyContainer = document.querySelector('.readyContainer');
+const startBtn = readyContainer.querySelector('.startBtn');
+const inputForm = document.querySelector('#inputForm');
+const input = inputForm.querySelector('input');
+const btnContainer = document.querySelector('.btnContainer');
+const nextBtn = btnContainer.querySelector('.nextBtn');
+const quitBtn = btnContainer.querySelector('.quitBtn');
+const right = document.querySelector('#right');
+const wrong = document.querySelector('#wrong');
+const result = document.querySelector('#result');
+const bar = document.querySelector('#bar');
 let answer;
 let start = false;
 let progressbarAction;
 
 function quit() {
-    reset(); //점수 및 결과 초기화용
-    const before = document.querySelector(".before");
-    const problem = document.querySelector(".problem");
-    before.classList.remove("hide");
-    problem.classList.add("hide");
-    //progressbar 초기화
+    reset();
+    const before = document.querySelector('.before');
+    const problem = document.querySelector('.problem');
+    before.classList.remove('hide');
+    problem.classList.add('hide');
     clearInterval(progressbarAction);
-    bar.style.width = "0%";
+    bar.style.width = '0%';
     start = false;
 }
 
 function reset() {
-    //다시 시작할 때 필요(점수 및 결과 초기화)
     right.innerHTML = 0;
     wrong.innerHTML = 0;
-    result.innerHTML = "";
+    result.innerHTML = '';
 }
 
 function paintScore(win) {
@@ -52,31 +43,28 @@ function paintScore(win) {
 }
 
 function paintResult(win) {
-    result.innerHTML = win ? "맞았습니다." : "틀렸습니다.";
+    result.innerHTML = win ? '맞았습니다.' : '틀렸습니다.';
     if (win) {
-        result.classList.add("rightColor");
-        result.classList.remove("wrongColor");
+        result.classList.add('rightColor');
+        result.classList.remove('wrongColor');
     } else {
-        result.classList.add("wrongColor");
-        result.classList.remove("rightColor");
+        result.classList.add('wrongColor');
+        result.classList.remove('rightColor');
     }
 }
 
 function checkNumber(e) {
     e.preventDefault();
-    //game start 전에 입력방지
     if (!start) {
-        input.value = "";
+        input.value = '';
         return;
     }
-
     const inputValue = Number(input.value);
-    //숫자외의 입력값 방지
     if (isNaN(inputValue)) {
-        input.value = "";
+        input.value = '';
         return;
     }
-    input.value = "";
+    input.value = '';
     let win = false;
     win = inputValue === answer ? true : false;
     paintScore(win);
@@ -86,8 +74,8 @@ function checkNumber(e) {
 }
 
 function startProgressbar() {
-    let width = 0; //progressbar scalewidth
-    progressbarAction = setInterval(function() {
+    let width = 0;
+    progressbarAction = setInterval(function () {
         if (width >= 100) {
             clearInterval(progressbarAction);
             paintResult(false);
@@ -103,15 +91,14 @@ function startProgressbar() {
 function makeQuiz(e) {
     clearInterval(progressbarAction);
     const targetName = e.target.className;
-    // console.log(targetName);
     checkNext(targetName);
     if (!start) return;
-    const before = document.querySelector(".before");
-    const problem = document.querySelector(".problem");
-    before.classList.add("hide");
-    problem.classList.remove("hide");
-    const firstNumber = document.querySelector("#firstNum");
-    const secondNumber = document.querySelector("#secondNum");
+    const before = document.querySelector('.before');
+    const problem = document.querySelector('.problem');
+    before.classList.add('hide');
+    problem.classList.remove('hide');
+    const firstNumber = document.querySelector('#firstNum');
+    const secondNumber = document.querySelector('#secondNum');
     const number1 = Math.ceil(Math.random() * 9);
     const number2 = Math.ceil(Math.random() * 9);
     firstNumber.innerHTML = number1;
@@ -122,29 +109,27 @@ function makeQuiz(e) {
 }
 
 function checkNext(targetName) {
-    //start하기전에 next를 누르는것 방지 : 처음시작을 체크
     if (!start) {
-        start = targetName.includes("startBtn") ? true : false;
+        start = targetName.includes('startBtn') ? true : false;
     } else {
-        //중간에 start button을 누르는 경우
-        if (targetName.includes("startBtn")) reset();
+        if (targetName.includes('startBtn')) reset();
     }
 }
 
 function startGame() {
-    startBtn.addEventListener("click", makeQuiz);
+    startBtn.addEventListener('click', makeQuiz);
 }
 
 function nextGame() {
-    nextBtn.addEventListener("click", makeQuiz);
+    nextBtn.addEventListener('click', makeQuiz);
 }
 
 function quitGame() {
-    quitBtn.addEventListener("click", quit);
+    quitBtn.addEventListener('click', quit);
 }
 
 function inputNumber() {
-    inputForm.addEventListener("submit", checkNumber);
+    inputForm.addEventListener('submit', checkNumber);
 }
 
 function init() {
