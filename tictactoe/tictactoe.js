9ca8975c9481fa06f11afd59b7f3a48d794bbedd
jjanmo <44132977+jjanmo@users.noreply.github.com>
2020-08-14 18:54:42 +0900

    Update code style of tictactoe game

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -4,18 +4,11 @@ const blocks = document.querySelectorAll('.block');
 const restartBtn = document.getElementById('js-restart-button');
 
 let blockData = null;
-/*
-{
-    key  / value
-    id  : status  o / x / ''
-}   
-*/
 let start = false;
 let player = 'x';
 let com = 'o';
-let curTurn = 'x'; //현재 턴
+let curTurn = 'x';
 let result = '';
-
 
 function makeRandomBlockId() {
     const row = Math.floor(Math.random() * 3);
@@ -30,7 +23,7 @@ function saveComputerData() {
         if (!blockData[id]) {
             selectable = true;
             saveData(id, com);
-            paintComputerTurn(id)
+            paintComputerTurn(id);
         }
     }
 }
@@ -40,7 +33,6 @@ function paintComputerTurn(id) {
     target.textContent = com;
     target.classList.add(`${com}`);
 
-    //승패검사 or 무승부 => 끝났는지
     if (checkEndGame()) changeTurn();
 }
 
@@ -63,7 +55,7 @@ function handlePlayer(e) {
         player = 'o';
         com = 'x';
     }
-    //player가 o이면 바로 시작 추가
+
     if (player === 'o') {
         start = true;
         blockChoosing();
@@ -71,43 +63,23 @@ function handlePlayer(e) {
     }
 }
 
-
-//2차원배열
-// function setBlockData() { 
-//     blockData = [[], [], []];
-//     let row = 0;
-//     blocks.forEach((ele, idx) => {
-//         if (idx !== 0 && idx % 3 === 0) row++;
-//         const obj = {
-//             id: ele.dataset.id,
-//             status: ''
-//         };
-//         blockData[row][idx % 3] = obj;
-//     })
-//     console.log(blockData);
-// }
-
-//그냥 배열
 function setBlockData() {
     blockData = {};
-    blocks.forEach(ele => {
+    blocks.forEach((ele) => {
         const id = ele.id;
-        blockData[id] = ''
+        blockData[id] = '';
     });
-    // console.log(blockData);
 }
 
-
-function changeTurn() { //턴은 항상 x => o 순으로 진행
+function changeTurn() {
     curTurn = curTurn === 'x' ? 'o' : 'x';
     msg.textContent = `${curTurn} turn`;
 }
 
-
 function handleClickBlock(e) {
     const { target } = e;
     const id = target.id;
-    if (curTurn === player) { //플레이어
+    if (curTurn === player) {
         if (!start) {
             blockChoosing();
             start = true;
@@ -115,7 +87,7 @@ function handleClickBlock(e) {
         target.textContent = player;
         target.classList.add(`${player}`);
         saveData(id, player);
-        //검사해줘야함 
+
         if (checkEndGame()) {
             changeTurn();
             setTimeout(saveComputerData, 1000);
@@ -125,13 +97,11 @@ function handleClickBlock(e) {
 
 function saveData(id, status) {
     const tmpBlockData = Object.assign({}, blockData);
-    tmpBlockData[id] = status
+    tmpBlockData[id] = status;
     blockData = tmpBlockData;
 }
 
-
 function checkEndGame() {
-    //com 승  / player 승  / draw
     const winnable = [
         ['00', '01', '02'],
         ['10', '11', '12'],
@@ -143,17 +113,20 @@ function checkEndGame() {
         ['02', '11', '20'],
     ];
 
-    winnable.forEach(locs => {
-        if (blockData[locs[0]] && blockData[locs[0]] === blockData[locs[1]] &&
-            blockData[locs[1]] && blockData[locs[1]] === blockData[locs[2]]) {
+    winnable.forEach((locs) => {
+        if (
+            blockData[locs[0]] &&
+            blockData[locs[0]] === blockData[locs[1]] &&
+            blockData[locs[1]] &&
+            blockData[locs[1]] === blockData[locs[2]]
+        ) {
             result = `${curTurn} win 😎`;
         }
     });
 
-    //승패가 안갈렸다면,,, 다음턴 or 무승부
     if (!result) {
         for (let id in blockData) {
-            if (!blockData[id]) { //빈칸 존재 => 다음턴
+            if (!blockData[id]) {
                 return 'next';
             }
         }
@@ -163,14 +136,12 @@ function checkEndGame() {
     blockClick();
 }
 
-//플레이어 선택 막기
 function blockChoosing() {
-    playersBtn.forEach(ele => ele.removeEventListener('click', handlePlayer));
+    playersBtn.forEach((ele) => ele.removeEventListener('click', handlePlayer));
 }
 
-//블럭 클릭 막기
 function blockClick() {
-    blocks.forEach(ele => ele.removeEventListener('click', handleClickBlock));
+    blocks.forEach((ele) => ele.removeEventListener('click', handleClickBlock));
 }
 
 function handleRestart(e) {
@@ -178,23 +149,22 @@ function handleRestart(e) {
     start = false;
     player = 'x';
     com = 'o';
-    curTurn = 'x'; //현재 턴
+    curTurn = 'x';
     result = '';
     msg.textContent = 'choose player or start';
-    blocks.forEach(ele => ele.textContent = '');
-    playersBtn.forEach(ele => ele.addEventListener('click', handlePlayer));
-    blocks.forEach(ele => ele.addEventListener('click', handleClickBlock));
-    blocks.forEach(ele => ele.className = 'block');
-    playersBtn.forEach(ele => ele.className = 'player');
+    blocks.forEach((ele) => (ele.textContent = ''));
+    playersBtn.forEach((ele) => ele.addEventListener('click', handlePlayer));
+    blocks.forEach((ele) => ele.addEventListener('click', handleClickBlock));
+    blocks.forEach((ele) => (ele.className = 'block'));
+    playersBtn.forEach((ele) => (ele.className = 'player'));
     playersBtn[0].classList.add('selected');
 }
 
 function init() {
-    playersBtn.forEach(ele => ele.addEventListener('click', handlePlayer));
-    blocks.forEach(ele => ele.addEventListener('click', handleClickBlock));
+    playersBtn.forEach((ele) => ele.addEventListener('click', handlePlayer));
+    blocks.forEach((ele) => ele.addEventListener('click', handleClickBlock));
     restartBtn.addEventListener('click', handleRestart);
     setBlockData();
 }
-
 
 init();
