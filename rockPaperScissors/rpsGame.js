cbc9082665826858212110eb6bfa3d0dd2dacf4f
jjanmo <44132977+jjanmo@users.noreply.github.com>
2020-08-14 18:52:28 +0900

    Update code style of rps game

--- rockPaperScissors/rpsGame.js
+++ rockPaperScissors/rpsGame.js
@@ -5,29 +5,23 @@ const buttonContainer = document.querySelector('.button-container');
 const restartBtn = document.querySelector('.restart-button');
 const startFlag = false;
 
-//game start : click button
 function choseRps() {
-    rps.forEach(ele => ele.addEventListener('click', userRps));
+    rps.forEach((ele) => ele.addEventListener('click', userRps));
 }
 
-//chose computer rps
 function getCompRps() {
     const comp = Math.floor(Math.random() * 3 + 1);
     return comp;
 }
 
-
-//compare comp and user
 function compare(user, comp) {
     if (user == comp) return 'draw';
-    else if (user == 1 && comp === 3 || user == 3 && comp === 1) {
+    else if ((user == 1 && comp === 3) || (user == 3 && comp === 1)) {
         return user > comp ? 'comp' : 'user';
-    }
-    else {
+    } else {
         return user > comp ? 'user' : 'comp';
     }
 }
-
 
 function changeScore(result) {
     const userScoreSpan = document.querySelector('#user-score');
@@ -37,80 +31,70 @@ function changeScore(result) {
     if (result === 'user') {
         userScore++;
         userScoreSpan.textContent = userScore;
-    }
-    else if (result === 'comp') {
+    } else if (result === 'comp') {
         computerScore++;
         computerScoreSpan.textContent = computerScore;
     }
 }
 
-
-function displayModal(compNum, result) {//compNum : 숫자 / result : 문자열
+function displayModal(compNum, result) {
     const h2 = modalContent.querySelector('h2');
     const icon = modalContent.querySelector('i');
     const p = modalContent.querySelector('p');
-
-    //모달창 열기
     modal.classList.remove('hidden');
 
-    //메세지변경
     if (result === 'draw') {
         h2.innerHTML = '비겼습니다😐';
-    }
-    else if (result === 'user') {
+    } else if (result === 'user') {
         h2.innerHTML = '당신이 이겼습니닷😎';
-    }
-    else {
+    } else {
         h2.innerHTML = '당신이 졌습니다.😥';
     }
 
-    //아이콘변경  및 컴퓨터 선택결과표시
     switch (compNum) {
-        case 1:
+        case 1: {
             icon.className = 'fas fa-hand-scissors fa-5x';
             p.innerHTML = '컴퓨터는 가위';
             break;
-        case 2:
+        }
+        case 2: {
             icon.className = 'fas fa-hand-rock fa-5x';
             p.innerHTML = '컴퓨터는 바위';
             break;
-        case 3:
+        }
+        case 3: {
             icon.className = 'fas fa-hand-paper fa-5x';
             p.innerHTML = '컴퓨터는 보';
             break;
+        }
     }
 }
 
-
 function userRps(e) {
     const chose = e.target.id;
-    console.log(chose);
     let userNum = 0;
     switch (chose) {
-        case 'scissors':
+        case 'scissors': {
             userNum = 1;
             break;
-        case 'rock':
+        }
+        case 'rock': {
             userNum = 2;
             break;
-        case 'paper':
+        }
+        case 'paper': {
             userNum = 3;
             break;
+        }
     }
 
-    //컴퓨터의 가위바위보 선택 결과
     const compNum = getCompRps();
-
-    //비교
     const result = compare(userNum, compNum);
 
-    //결과출력
-    //1) 점수바꾸고  / 2) 모달창 띄우기
     changeScore(result);
     displayModal(compNum, result);
 }
 
-//다시 시작하기 초기화
 function restart() {
     const userScoreSpan = document.querySelector('#user-score');
     const computerScoreSpan = document.querySelector('#computer-score');
@@ -125,7 +109,6 @@ function closeModal() {
         startFlag = true;
     }
 }
-
 
 function init() {
     choseRps();
