e81cdee4c6df079ab4b97a5edd822258eb55d12a
jjanmo <jjanmo07@hanmail.net>
2023-11-25 16:29:43 +0900

    feat: 정적파일 경로 설정 및 라우팅 재설정

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const app = express();
 
 const PORT = 3030;
 
-app.set('views', `${process.cwd()}/src`);
+app.set('views', `${process.cwd()}/src/views`);
 app.set('view engine', 'ejs');
 app.engine('html', ejs.renderFile);
 
-app.get('/', (req, res) => res.render('home.html'));
-app.get('/vampire', (req, res) => res.render('vampire/index.html'));
+app.use(express.static(`${process.cwd()}/src/public`));
+
+app.get('/', (_, res) => res.render('home.html'));
+app.get('/vampire', (_, res) => res.render('vampire.html'));
 
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
