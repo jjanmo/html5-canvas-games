b22598ac04e8df16fdeada70ba24a31c83114471
jjanmo <jjanmo07@hanmail.net>
2023-11-29 23:38:19 +0900

    feat: add mousemove event on card

--- src/public/home/app.js
+++ src/public/home/app.js
@@ -1,3 +1,19 @@
 import './styles.css';
 
-console.log('home');
+const cards = document.querySelectorAll('.card');
+cards.forEach((card) => {
+  card.addEventListener('mousemove', (e) => {
+    const cardElem = e.currentTarget;
+    const { left, top } = cardElem.getBoundingClientRect();
+    const { clientX, clientY } = e;
+
+    const mouseX = clientX - left;
+    const mouseY = clientY - top;
+
+    cardElem.style.background = `radial-gradient(circle at ${mouseX}px ${mouseY}px, rgb(116, 185, 255) 0%, rgba(116,185,255,0) calc(0% + 200px)  )`;
+  });
+
+  card.addEventListener('mouseout', (e) => {
+    e.currentTarget.removeAttribute('style');
+  });
+});
